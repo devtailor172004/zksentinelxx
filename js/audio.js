90759d5c77b53f2e40093b2517e2225468fd43d6
Dev Tailor <126579749+devtailor172004@users.noreply.github.com>
2026-01-17 02:25:57 +0530

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,25 +1,15 @@
-// js/audio.js
-
 class AudioEngine {
     constructor() {
-        // Basic constructor. We will expand this later.
         this.masterVolume = 0.7;
     }
-
-    // Mock play functions so no errors are thrown.
-    // We will build this out properly in a later phase.
     playBackgroundMusic(theme) {
-        console.log(`AudioEngine: Playing background music for theme: ${theme}`);
+        console.log(`AudioEngine: Mock playing background music for theme: ${theme}`);
     }
-
     playSound(soundName) {
-        console.log(`AudioEngine: Playing sound: ${soundName}`);
+        console.log(`AudioEngine: Mock playing sound: ${soundName}`);
     }
-
     stopAll() {
-        console.log("AudioEngine: Stopping all sounds.");
+        console.log("AudioEngine: Mock stopping all sounds.");
     }
 }
-
-// Make the class available globally to prevent script errors
 window.AudioEngine = AudioEngine;
